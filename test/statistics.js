4552fa89e2cc6260555caad2cce5add0ef0e19a3
Magnus Nilsson <magnus.nilsson@futurice.com>
2018-02-08 19:20:35 +0100

    Oops

--- test/statistics.js
+++ test/statistics.js
@@ -4,11 +4,9 @@ var statistics = require("../app/statistics");
 describe("Statistical functions", function() {
   describe("Avg calculation", function() {
     it("calculates the average of an array of numbers", function() {
-      var posAvg = statistics.avg([0, 1, 2, 3]);
-      var negAvg = statistics.avg([0, -1, -2, -3]);
+      var posAvg = statistics.avg([0, 1, 2, 3, 4]);
 
-      expect(posAvg).to.equal(3);
-      expect(negAvg).to.equal(-3);
+      expect(posAvg).to.equal(2);
     });
   });
 
